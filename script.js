77d426693fb5c3cf47783ed060c1ba24a178896b
YugeshKK <95625392+YugeshKK@users.noreply.github.com>
2023-09-29 14:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const p= document.getElementById('time')
 const date=new Date();
 var hor=date.getHours()
 var ampm = hor >= 12 ? 'PM' : 'AM';
-hor= hor+12-24;
+hor= hor%12;
 p.innerText=date.getMonth() +"/"+ date.getDay()+"/"+date.getFullYear()+","+hor+":"+date.getMinutes()+':'+date.getSeconds()+" "+ampm
